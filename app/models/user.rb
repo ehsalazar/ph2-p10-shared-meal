805d729a8362f72e9a39e9aa531de26b266b4b78
Ernie Salazar <ehsalazar75@gmail.com>
2014-07-27 21:08:04 -0700

    Add bcrypt for user password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   has_many :events
+
+  include BCrypt
+  def password
+    @password ||= Password.new(password_hash)
+  end
+  def password=(new_password)
+    if new_password.length < 6
+      self.errors.add(:password, "is invalid (minimum is 6 characters).")
+    else
+      @password = Password.create(new_password)
+      self.password_hash = @password
+    end
+  end
+
 end
