2ca2fdd6dc2a4a0a6cd35834f2e089ad931d7cfa
Ernie Salazar <ehsalazar75@gmail.com>
2014-07-28 18:04:55 -0700

    Updated migration file to fix typo

--- db/migrate/20140727195116_create_events.rb
+++ db/migrate/20140727195116_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string :location, :comments,
+      t.string :location, :comments
       t.date :date
       t.boolean :active, default: false
       t.belongs_to :user
