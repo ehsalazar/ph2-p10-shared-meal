28aeb5a8cff7a963cb41c2a35338aa7de9dcd680
Apprentice <ehsalazar75@gmail.com>
2014-07-29 17:20:50 -0700

    Updated meals migration to include time

--- db/migrate/20140727195116_create_meals.rb
+++ db/migrate/20140727195116_create_meals.rb
@@ -1,8 +1,7 @@
 class CreateMeals < ActiveRecord::Migration
   def change
     create_table :meals do |t|
-      t.string :location, :comments
-      t.date :date
+      t.string :location, :comments, :date, :time
       t.boolean :active, default: false
       t.belongs_to :user
       t.timestamps
